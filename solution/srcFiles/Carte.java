c979bca26ff39e39b9d1fda0ce820c8c9a2da168
dizco <gabriel.bourgault@kiosoft.ca>
2016-11-17 21:55:16 -0500

    Refactor méthode equilibrerZones

--- solution/srcFiles/Carte.java
+++ solution/srcFiles/Carte.java
@@ -61,67 +61,73 @@ public class Carte {
         }
 
         if (inegal){
-            ArrayList<Integer> differentiel = new ArrayList<>();
-
-            double moyenne = calculateAverage(nombreDeVehicules);
-            int up = (int)Math.ceil(moyenne);
-            int down = (int)Math.floor(moyenne);
-
-            for (int i = 0; i < zones.size(); i++){
-                //int temp = Math.min(Math.abs(nombreDeVehicules.get(i)-up), Math.abs(nombreDeVehicules.get(i)-down));
-
-                if (down > nombreDeVehicules.get(i)) //il n'y a pas assez de véhicules
-                    differentiel.add(i, -1 * (down - nombreDeVehicules.get(i)));
-                else if (up < nombreDeVehicules.get(i)) //il y a trop de véhicules
-                    differentiel.add(i, nombreDeVehicules.get(i) - up);
-                else //il y a un nombre acceptable de véhicules
-                    differentiel.add(i, 0);
-            }
-
-            ArrayList<Vehicule> vehiculesExcedentaires = new ArrayList<>();
-            for (int i = 0; i < zones.size(); i++){
-                while (differentiel.get(i) > 0){
-                    differentiel.set(i, differentiel.get(i) - 1);
-                    vehiculesExcedentaires.add(getVehiculeRandomDansZone(zones.get(i)));
-                }
-            }
-
-            while (Math.abs(calculerSomme(differentiel)) > vehiculesExcedentaires.size()) {
-                //on a besoin de plus de véhicules que ceux que l'on a
-                boolean vehiculeTrouve = false;
-                for (int i = 0; i < zones.size() && !vehiculeTrouve; i++){
-                    if (nombreDeVehiculesDansZone(zones.get(i)) == up){
-                        vehiculesExcedentaires.add(getVehiculeRandomDansZone(zones.get(i)));
-                        vehiculeTrouve = true;
-                    }
-                }
-            }
-
-            for (int i = 0; i < zones.size(); i++){
-                while (differentiel.get(i) < 0){
-                    if (vehiculesExcedentaires.size() == 0){
-                        throw new NoSuchElementException();
-                    }
-
-                    differentiel.set(i, differentiel.get(i) + 1);
-                    Vehicule v = vehiculesExcedentaires.remove(0);
-                    v.setZoneActuelle(zones.get(i));
-                    v.setPositionActuelle(zones.get(i).getDestinationRandom());
-                }
-            }
-
-            /*ArrayList<Integer> nombreDeVehicules2 = new ArrayList<>();
-            for (int i = 0; i < zones.size(); i++){
-                int nombre = nombreDeVehiculesDansZone(zones.get(i));
-                nombreDeVehicules2.add(nombre);
-            }
-
-            int i = 0;*/
-
+            repartirLesVehiculesEntreLesZones(nombreDeVehicules);
         }
     }
 
-    //Tiré de : http://stackoverflow.com/a/10791597/6316091
+    private void repartirLesVehiculesEntreLesZones(ArrayList<Integer> nombreDeVehicules){
+        ArrayList<Integer> differentiel = calculerDifferentiel(nombreDeVehicules);
+
+        ArrayList<Vehicule> vehiculesExcedentaires = new ArrayList<>();
+        for (int i = 0; i < zones.size(); i++){
+            while (differentiel.get(i) > 0){
+                differentiel.set(i, differentiel.get(i) - 1);
+                vehiculesExcedentaires.add(getVehiculeRandomDansZone(zones.get(i)));
+            }
+        }
+
+        while (Math.abs(calculerSomme(differentiel)) > vehiculesExcedentaires.size()) {
+            //on a besoin de plus de véhicules que ceux que l'on a
+            boolean vehiculeTrouve = false;
+            for (int i = 0; i < zones.size() && !vehiculeTrouve; i++){
+                if (nombreDeVehiculesDansZone(zones.get(i)) == (int)Math.ceil(calculateAverage(nombreDeVehicules))){
+                    vehiculesExcedentaires.add(getVehiculeRandomDansZone(zones.get(i)));
+                    vehiculeTrouve = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < zones.size(); i++){
+            while (differentiel.get(i) < 0){
+                if (vehiculesExcedentaires.size() == 0){
+                    throw new NoSuchElementException();
+                }
+
+                differentiel.set(i, differentiel.get(i) + 1);
+                Vehicule v = vehiculesExcedentaires.remove(0);
+                v.setZoneActuelle(zones.get(i));
+                v.setPositionActuelle(zones.get(i).getDestinationRandom());
+            }
+        }
+
+        /*ArrayList<Integer> nombreDeVehicules2 = new ArrayList<>();
+        for (int i = 0; i < zones.size(); i++){
+            int nombre = nombreDeVehiculesDansZone(zones.get(i));
+            nombreDeVehicules2.add(nombre);
+        }
+
+        int i = 0;*/
+    }
+
+    private ArrayList<Integer> calculerDifferentiel(ArrayList<Integer> nombreDeVehicules) {
+        ArrayList<Integer> differentiel = new ArrayList<>();
+        double moyenne = calculateAverage(nombreDeVehicules);
+        int up = (int)Math.ceil(moyenne);
+        int down = (int)Math.floor(moyenne);
+
+        for (int i = 0; i < zones.size(); i++){
+            //int temp = Math.min(Math.abs(nombreDeVehicules.get(i)-up), Math.abs(nombreDeVehicules.get(i)-down));
+            if (down > nombreDeVehicules.get(i)) //il n'y a pas assez de véhicules
+                differentiel.add(i, -1 * (down - nombreDeVehicules.get(i)));
+            else if (up < nombreDeVehicules.get(i)) //il y a trop de véhicules
+                differentiel.add(i, nombreDeVehicules.get(i) - up);
+            else //il y a un nombre acceptable de véhicules
+                differentiel.add(i, 0);
+        }
+        return differentiel;
+    }
+
+    //Inspiré de : http://stackoverflow.com/a/10791597/6316091
     private double calculateAverage(ArrayList <Integer> liste) {
         Integer sum = 0;
         if(!liste.isEmpty()) {
